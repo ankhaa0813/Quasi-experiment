579043fe2011935af35fcdaa28c956556ff50d27
ankhaa0813 <66380003+ankhaa0813@users.noreply.github.com>
2022-07-08 15:13:06 +0200

    cahnge

--- Difference in Difference/dd.py
+++ Difference in Difference/dd.py
@@ -1,40 +1,40 @@
-
+#Code for Difference and Difference approach and You will find information about context of the data and methodology from README file 
 # for data cleaning and analysis 
-from sre_parse import DIGITS
 import pandas as pd 
 import numpy as np
 # These libraries will be used for main econometric analysis 
 import statsmodels as sm 
 import statsmodels.formula.api as smf
 # main data will be called from this library which is also available in R software with same name
-# it is very easy accessible from different development environment such as JupiterNote, VScode
+# this data is very easy to implement DinD approach without data cleaning
 import wooldridge
-#for visualiazation 
+#for visualiazation of the data
 import matplotlib.pyplot as plt
-# calling data]
-# Information about  DATA and METHODOLOGY are found from README file
-housing=wooldridge.data('kielmc')
 # #data descriptionn
 housing = wooldridge.data('kielmc', description=True)
+# calling data
+# Information about  DATA and METHODOLOGY are found from README file
+housing=wooldridge.data('kielmc')
 # save it as data frame using panda
 house=pd.DataFrame(housing)
-# to see the first few rows of data
+# to see the first few rows of data to inspect it
 house.head
-# dummy variable plays important role in DinD estimation, u can create dummy variable same as below. But we don't use it now because 
-# we have already pcreated variables in dataset we downloaded from wooldridge library
+# dummy variable plays an important role in DinD estimation, u can create dummy variable same as below. But we don't use it now because 
+# we have already pre-built variables in dataset we downloaded from wooldridge library
+# pd.get_dummies(house.year)
 # it is easy to define estimation equation beforehand. We can just create interaction term with * sign within formula like R
 basic ='rprice~y81+nearinc+y81*nearinc'
-# I don't want waste time on explaining variables which can be found from result of code in line 15
+# I don't want waste time on explaining variables which can be found from result of code in line 15 or from the original paper
 r_basic = smf.ols(basic,data=house ).fit()
 #r_basic = smf.ols(rprice~y81+nearinc+y81*nearinc,data=house ).fit() # you can also do it without formula
 r_basic.summary() # we can find exact same result with Wooldridge book
-# I hope u can explain the result. otherwise you can find from the Wooldridge book. 
+# I hope u can explain the result. otherwise you can find from the Wooldridge book or the original paper 
 #Adding some controls
 basic_control ='rprice~y81+nearinc+y81*nearinc+age+agesq'
 # I don't want waste time on explaining variables which can be found from result of code in line 15
 r_basic_control = smf.ols(basic_control,data=house ).fit()
 r_basic_control.summary()
-# Drawing graph showing mean of house price before and after the rumor
+# Drawing graph showing the mean of house price before and after the rumor
 hous=house[['year','nearinc','rprice']] #creating subsample to make process easy
 y=hous.groupby(['year','nearinc']).mean() #finding mean of subsample using year and nearinc with panda library
 plot1=y.reset_index() #formating table index
@@ -48,3 +48,8 @@ plot1.set_index('year', inplace=True) #defining index of the plot or x axis
 plt.axvline(x = 1979, color = 'b', label = '') # drawing vertical line
 plot1.groupby('nearinc')['rprice'].plot(legend=True)  # main part for the plot assigning main variable that we want to see in the graph
 plt.show()
+# You can also conduct some placebo estimations as a robustness check
+# here I implemented place test using score of neighborhood. Conceptually, rumor about a new garbage incinerator doesn't have to effect score of neighborhood
+placebo =' nbh~y81+nearinc+y81*nearinc'
+r_placebo = smf.ols(placebo,data=house ).fit()
+r_placebo.summary()
--- Instrumental Variable/IV.py
+++ Instrumental Variable/IV.py
@@ -1,4 +1,4 @@
-
+#Code for Instrumental variable approach and You will find information about context of the data and methodology from README file 
 # for data cleaning and analysis 
 import pandas as pd 
 import numpy as np
@@ -7,10 +7,10 @@ import statsmodels as sm
 import statsmodels.formula.api as smf
 import linearmodels.iv as IV
 # main data will be called from this library which is also available in R software with same name
-# it is very easy accessible from different development environment such as JupiterNote, VScode
 import wooldridge
 #for visualiazation 
 import matplotlib.pyplot as plt
+import seaborn as sn
 # calling data and its description
 card=wooldridge.data('card')
 # save it as data frame using panda
@@ -24,6 +24,11 @@ fst_for ='educ~nearc4+IQ+exper+black+married+fatheduc+motheduc'
 # with several potantial control variables
 fst_sta =smf.ols(fst_for,data=card ).fit()
 fst_sta.summary() # to see the result in table
+# In order to check whether our instumental variable satisfies relevance assumption we can see the correlation matrix of the dataframe
+corr_data=card[['lwage', 'educ','nearc4', 'black', 'married', 'fatheduc', 'motheduc']] # getting subsample to make correlation matrix readable
+corr_mat=corr_data.corr() #calculating corr matrix and saving it to corr_mat
+sn.heatmap(corr_mat, annot=True) #drawing graph
+plt.show() # show correaltoin matrix
 #extracting fitted values from the 1st stage equation
 educ_hat=fst_sta.fittedvalues
 #2nd stage
@@ -31,9 +36,14 @@ sec_for ='lwage~educ_hat+IQ+exper+black+married+fatheduc+motheduc'
 sec_sta =smf.ols(sec_for,data=card ).fit()
 sec_sta.summary()  
 # In order to implement IV regression with library, you need to use bracket for your endogenous variable, 
-# for this case educ, and isntrumenttal variable, nearc4
+# for this case educ and isntrumenttal variable, nearc4
 main_equation='lwage~1+IQ+exper+black+married+fatheduc+motheduc+[educ~nearc4]'
 main_results =IV.IV2SLS.from_formula(main_equation,data=card ).fit()
 # you can also imlement estimation without defining equation before
 #main_results =IV.IV2SLS.from_formula(dependent=card['lwage'], exog=card[['IQ','exper','black','married', 'fatheduc' 'motheduc']], endog=card['educ'], instruments=card['nearc4']).fit()
 main_results.summary
+# in order to check the exclusion restriction, we can make some placebo test with different variable. 
+# But it should be noted that checking exclusion restriction is very difficult to check emprically
+placebo_equation='lwage~1+IQ+exper+black+married+fatheduc+motheduc+[educ~nearc2]' # here I used growing up near 2 years college as instrument for the educatio as purpose of place test
+placebo_results =IV.IV2SLS.from_formula(placebo_equation,data=card ).fit()
+placebo_results.summary
